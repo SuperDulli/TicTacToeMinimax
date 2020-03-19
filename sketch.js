22dbe76b09346afbbe3095985348321da503fb5b
Chris Helmerding <chris.helmerding@gamil.com>
2020-03-19 14:40:39 +0100

    ready to play manually

--- sketch.js
+++ sketch.js
@@ -1,15 +1,40 @@
 let board;
+const players = ['X', 'O'];
+
+let currentPlayer;
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
   background(255);
-  board = new Board(3,3);
+  board = new Board(3, 3);
+  currentPlayer = players[0];
+
 }
 
 function draw() {
   board.draw();
 }
 
+function gameOver(winner) {
+  noLoop();
+  let resultP = createP('');
+  resultP.style('font-size', '32pt');
+  if (winner === 'tie') {
+    resultP.html('Tie!');
+  } else {
+    resultP.html(`${winner} wins!`);
+  }
+
+}
+
 function mousePressed() {
-  board.getClickedTile(mouseX, mouseY);
+  const tile = board.getClickedTile(mouseX, mouseY);
+  if (board.get(tile.i, tile.j) === '') {
+    board.set(tile.i, tile.j, currentPlayer)
+    const winner = board.checkWinner();
+    if (winner) {
+      gameOver(winner);
+    }
+    currentPlayer = (currentPlayer === 'X') ? 'O' : 'X'; // switch active Player
+  }
 }
--- tictactoe.js
+++ tictactoe.js
@@ -10,6 +10,9 @@ class Board {
       }
       this.board.push(line);
     }
+    this._available = this.rows * this.cols;
+    this._fieldHeight = height / this.rows;
+    this._fieldWidth = width / this.cols;
   }
 
   get(i, j) {
@@ -18,16 +21,62 @@ class Board {
 
   set(i, j, value) {
     this.board[i][j] = value;
+    this._available -= 1;
   }
 
+  _fieldHeight;
+
   getClickedTile(mouseX, mouseY) {
-    const i = Math.floor(mouseY / (height / this.rows));
-    const j = Math.floor(mouseX / (width / this.cols));
-    console.log(`Clicked on tile: ${i}, ${j}: ${this.get(i,j)}`)
+    const i = Math.floor(mouseY / this._fieldHeight);
+    const j = Math.floor(mouseX / this._fieldWidth);
+    console.log(`Clicked on tile: ${i}, ${j}: ${this.get(i, j)}`);
+    return {i, j};
+  }
+
+  checkWinner() {
+    let winner = null;
+    // Horizontal
+    for (let i = 0; i < this.rows; i++) {
+      if (allEqual(this.board[i])) {
+        winner = this.board[i][0];
+      }
+    }
+
+    // Vertical
+    for (let i = 0; i < this.cols; i++) {
+      if (allEqual(arrayColumn(this.board, i))) {
+        winner = this.board[0][i];
+      }
+    }
+
+    // Diagonal
+    if (this.rows !== this.cols) {
+      console.error("can not check diagonally. Board is not a square!")
+      return winner;
+    }
+    let d1 = [];
+    let d2 = [];
+    for (let i = 0; i < this.rows; i++) {
+      d1.push(this.board[i][i]);
+      d2.push(this.board[this.rows - i - 1][this.cols - i - 1]);
+    }
+    if (allEqual(d1)) {
+      winner = d1[0];
+    } else if (allEqual(d2)) {
+      winner = d2[0];
+    }
+
+    // check for tie
+    if (winner == null && this._available === 0) {
+      return 'tie'
+    } else {
+      return winner;
+    }
   }
 
   draw() {
     stroke(0);
+    // draw layout
     for (let i = 1; i < this.rows; i++) {
       for (let j = 1; j < this.cols; j++) {
         const xCor = (width / this.cols) * j;
@@ -36,5 +85,27 @@ class Board {
       const yCor = (height / this.rows) * i;
       line(0, yCor, width, yCor);
     }
+
+    // draw the contents
+    for (let i = 0; i < this.rows; i++) {
+      for (let j = 0; j < this.cols; j++) {
+        const spot = this.board[i][j];
+        // center pf current field
+        const x = this._fieldWidth * j + this._fieldWidth / 2;
+        const y = this._fieldHeight * i + this._fieldHeight / 2;
+        const r = this._fieldWidth / 4; // radius
+        if (spot === 'X') {
+          line(x - r, y - r, x + r, y + r);
+          line(x + r, y - r, x - r, y + r);
+        } else if (spot === 'O') {
+          noFill();
+          ellipse(x, y, r * 2);
+        }
+      }
+    }
   }
 }
+
+const allEqual = arr => arr.every(v => v === arr[0] && v !== '');
+
+const arrayColumn = (arr, n) => arr.map(x => x[n]);
